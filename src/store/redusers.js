5e02a96cc75d2f5779fe9449e2e6499fb5ff0062
Syuzan <syuzanna.ant@gmail.com>
2021-01-14 10:46:12 +0400

    add

--- src/store/redusers.js
+++ src/store/redusers.js
@@ -1,23 +1,26 @@
-
-import {ACTION_TYPE_FIRST_NAME, ACTION_TYPE_LAST_NAME, INCREMENT, DECREMENT} from './actions';
+import {
+  ACTION_TYPE_FIRST_NAME,
+  ACTION_TYPE_LAST_NAME,
+  INCREMENT,
+  DECREMENT,
+} from "./actions";
 
 const initialState = {
-    firstName: '',
-    lastName: '',
-    count: 0
-  }
+  firstName: "",
+  lastName: "",
+  count: 0,
+};
 
-export const rootReduser = ( state = initialState , action) => {
-      switch(action.type){
-            case ACTION_TYPE_FIRST_NAME:
-              return{ ...state , firstName:action.payload };
-            case ACTION_TYPE_LAST_NAME:
-              return{ ...state , lastName:action.payload };
-            case INCREMENT:
-              return{ ...state, count:action.payload }
-            case DECREMENT:
-              return{ ...state, count:action.payload }
-      }
-    return state
+export const rootReduser = (state = initialState, action) => {
+  switch (action.type) {
+    case ACTION_TYPE_FIRST_NAME:
+      return { ...state, firstName: action.payload };
+    case ACTION_TYPE_LAST_NAME:
+      return { ...state, lastName: action.payload };
+    case INCREMENT:
+      return { ...state, count: action.payload };
+    case DECREMENT:
+      return { ...state, count: action.payload };
   }
-  
+  return state;
+};
